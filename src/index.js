8467dfc18b5f8580ee8426e79f6703dd752bc44b
Nastassia Arkharava <nastassiaarkharava@gmail.com>
2023-01-10 16:26:24 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let res = '';
+
+	for (let i = 0; i < str.length; i++) {
+		for (let j = 0; j < bracketsConfig.length; j++) {
+			if (str[i] === bracketsConfig[j][0]) {
+				if (bracketsConfig[j][0] === bracketsConfig[j][1]) {
+					if (res[res.length - 1] === bracketsConfig[j][0]) {
+						res = res.substring(0, res.length - 1);
+					} else res += str[i];
+				} else {
+					res += str[i];
+				}
+			} else if (str[i] === bracketsConfig[j][1]) {
+				if (res[res.length - 1] === bracketsConfig[j][0]) {
+					res = res.substring(0, res.length - 1);
+				} else return false;
+			}
+		}
+	}
+
+	return res ? false : true;
 }
